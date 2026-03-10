8a232fc7ac0855cfd554f9224eee397f3444d77d
Asmit Nayak <asmit.nayak@gmail.com>
2026-03-10 21:08:21 +0100

    Change base path to '/new_wipi_website_2/'
    Updated the base path for the website configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from 'astro/config';
 // TODO: Replace 'YOUR_USERNAME' with your GitHub username
 export default defineConfig({
   site: 'https://wiscprivacy.com',
-  base: '/new_wipi_website/',
+  base: '/new_wipi_website_2/',
   output: 'static',
   build: {
     assets: 'assets'
   }
 });
+
 
